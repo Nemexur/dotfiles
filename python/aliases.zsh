3fb0917daa1a6af8327b0571f28283deae642db2
nemexur <alex.milogradsky@gmail.com>
2020-07-17 12:12:28 +0300

    Changed python aliases

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,11 +1,23 @@
 alias jupstart='jupyter lab'
-alias crvenv='python3 -m venv'
 
-setKernel() {
+crvenv() {
+    local python_ver=3
+    local venv_nm=venv
+    while [[ "$#" -gt 0 ]]
+    do
+        case $1 in
+        -v|--version) local python_ver=${2:-3} ;;
+        -n|--name) local venv_nm=${2:-venv} ;;
+        esac
+        shift
+    done
+    python$python_ver -m venv $venv_nm
+}
+setkernel() {
     pip install ipykernel
     python -m ipykernel install --user --name "$1" --display-name "$2"
 }
-jupstartscr() {
+jupscr() {
     echoInfo "Creating screen `cur_dir`_screen"
     screen -dmS `cur_dir`_screen jupyter lab
 }
