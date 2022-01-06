df7d16feeb38ae9a86a866a60dd62bd4591ca435
nemexur <alex.milogradsky@gmail.com>
2022-01-07 02:19:15 +0300

    Fix dotfiles/bin

--- files/system/_path.zsh
+++ files/system/_path.zsh
@@ -1,2 +1,2 @@
-export PATH="./bin:/usr/local/bin:/usr/local/sbin:$_ZSH/bin:$ZSH/bin:$PATH"
+export PATH="./bin:/usr/local/bin:/usr/local/sbin:$_ZSH/files/bin:$ZSH/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
