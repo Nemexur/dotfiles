c455840e735070026369d3db9a5bc138da0c0be9
nemexur <alex.milogradsky@gmail.com>
2022-01-23 01:11:35 +0300

    Update aliases

--- files/system/aliases.zsh
+++ files/system/aliases.zsh
@@ -2,12 +2,6 @@
 #   Made possible through contributions from generous benefactors like
 #   `brew install coreutils`
 
-# if $(gls &>/dev/null); then
-#   alias ls="gls -F --color"
-#   alias l="gls -lAh --color"
-#   alias ll="gls -l --color"
-#   alias la='gls -A --color'
-# else
 alias ll='colorls -la'
 alias ls='colorls --sd'
 alias la='colorls --sd -A'
