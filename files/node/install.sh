05013178ce4317742686653272d76e62970fe4c1
nemexur <alex.milogradsky@gmail.com>
2022-05-24 19:34:42 +0300

    Update install script for npm

--- files/node/install.sh
+++ files/node/install.sh
@@ -5,9 +5,15 @@
 # This installs some of the needed packages from npm.
 
 if ! command -v spoof &> /dev/null; then
-  npm install spoof -g
+  npm install -g spoof
 else
   echo " [SKIP] Spoof is already installed. "
+fi
+
+if ! command -v prettier &> /dev/null; then
+  npm install -g prettier @prettier/plugin-php prettier-plugin-solidity
+else
+  echo " [SKIP] Prettier is already installed. "
 fi
 
 exit 0
