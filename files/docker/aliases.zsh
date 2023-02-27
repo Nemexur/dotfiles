c57c110ce41211bdf98a409ebe3e2b31f2797948
nemexur <alex.milogradsky@gmail.com>
2023-02-27 22:52:12 +0300

    New aliases

--- files/docker/aliases.zsh
+++ files/docker/aliases.zsh
@@ -1,5 +1,5 @@
 alias d='docker $*'
-alias d-c='docker-compose $*'
+alias dcompose='docker-compose $*'
 alias ddelstopped='docker rm $(docker ps -a -q)'
 alias dclean='docker system prune -a'
 alias dhardclean='docker volume ls -qf dangling=true | xargs docker volume rm'
--- files/system/_path.zsh
+++ files/system/_path.zsh
@@ -1,4 +1,4 @@
-export PATH="${PATH}:./bin:/usr/local/bin/node:/usr/local/bin:/usr/local/sbin:$DOTFILES/files/bin:$ZSH/bin"
+export PATH="${PATH}:./bin:/usr/local/bin/node:/usr/local/bin:/usr/local/sbin:$DOTFILES/files/bin:$ZSH/bin:$HOME/.local/bin"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
     export PATH="${PATH}:/home/linuxbrew/.linuxbrew/bin"
