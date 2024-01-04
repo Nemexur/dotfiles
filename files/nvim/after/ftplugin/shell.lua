00dce4f70f4326447cb7224e8406837c81b92062
nemexur <alex.milogradsky@gmail.com>
2024-01-05 02:20:50 +0300

    feat: remove ftplugin/shell.lua

--- files/nvim/after/ftplugin/shell.lua
+++
@@ -1,7 +0,0 @@
-local lspconfig_ok, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_ok then
-    return
-end
-
-vim.filetype.add({ extension = { zsh = "zsh" } })
-lspconfig.bashls.setup({ filetypes = { "sh", "zsh", "bash" } })
