716470e3c4f2dac5d9b1840f1e32358e2ecab1d6
nemexur <alex.milogradsky@gmail.com>
2024-07-11 11:41:26 +0300

    fix(zsh): remove duplicate of reload function

--- files/system/aliases.zsh
+++ files/system/aliases.zsh
@@ -55,13 +55,3 @@ else
 fi
 alias pbc='pbcopy'
 alias pbp='pbpaste'
-
-function reload {
-	# Delete current completion cache
-	command rm -f $_comp_dumpfile $ZSH_COMPDUMP
-
-	# Old zsh versions don't have ZSH_ARGZERO
-	local zsh="${ZSH_ARGZERO:-${functrace[-1]%:*}}"
-	# Check whether to run a login shell
-	[[ "$zsh" = -* || -o login ]] && exec -l "${zsh#-}" || exec "$zsh"
-}
