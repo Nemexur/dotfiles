4d1307b82b2e85fbb1320ce8b1b854625c72cd67
nemexur <alex.milogradsky@gmail.com>
2024-07-11 10:53:06 +0300

    refactor(zsh): make it faster

--- files/system/path.zsh
+++ files/system/path.zsh
@@ -1,18 +1,15 @@
 export PATH="./bin:/usr/local/bin/node:/usr/local/bin:/usr/local/sbin:$DOTFILES/files/bin:$HOME/.local/bin:$HOME/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 export CDPATH=.:..:$HOME:$GOROOT/src:$GOPATH/src/golang.org:$GOPATH/src/github.com
-if [[ "${OSTYPE}" == "linux-gnu"* ]]; then
-    export PATH="${PATH}:/home/linuxbrew/.linuxbrew/bin"
-fi
 # GNU tools
-export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gawk/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-indent/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-tar/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-time/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-which/libexec/gnubin:$PATH"
-export PATH="/opt/homebrew/opt/gnu-units/libexec/gnubin:$PATH"
-# PyEnv
-export PATH="$PYENV_ROOT/bin:$PATH"
+if [[ "${OSTYPE}" == "darwin"* ]]; then
+	export PATH="${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gawk/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-getopt/bin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-indent/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-tar/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-time/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-which/libexec/gnubin:$PATH"
+	export PATH="${HOMEBREW_PREFIX}/opt/gnu-units/libexec/gnubin:$PATH"
+fi
