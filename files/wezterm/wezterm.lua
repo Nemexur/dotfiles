d293d981c16b0d97376431ccb2978d3f82cf5024
nemexur <alex.milogradsky@gmail.com>
2024-07-19 16:36:51 +0300

    refactor(wezterm): simplify config

--- files/wezterm/wezterm.lua
+++ files/wezterm/wezterm.lua
@@ -1,16 +1,31 @@
 local wezterm = require("wezterm")
 
-local function update(cfg, c)
-    for k, v in pairs(c) do
-        cfg[k] = v
-    end
-end
-
 local config = {}
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
-update(config, require("appearance"))
-update(config, require("fonts"))
+
+-- Appearance
+config.color_scheme = "Dracula+"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_background_opacity = 0.80
+config.macos_window_background_blur = 8
+config.max_fps = 120
+config.scrollback_lines = 15000
+config.inactive_pane_hsb = {
+    saturation = 0.9,
+    brightness = 0.6,
+}
+-- Font
+config.font = wezterm.font("JetBrainsMono NF")
+config.font_size = 15.0
+config.adjust_window_size_when_changing_font_size = false
+config.warn_about_missing_glyphs = false
+config.harfbuzz_features = {
+    "zero", -- Use a slashed zero '0' (instead of dotted)
+    "kern", -- (default) kerning (todo check what is really is)
+    "liga", -- (default) ligatures
+    "clig", -- (default) contextual ligatures
+}
 
 return config
