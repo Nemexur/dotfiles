b75cce53f7d58e0819b25a6fd4c5f576de120f4f
nemexur <alex.milogradsky@gmail.com>
2026-03-29 11:10:08 +0300

    fix(tmux): warning regarding regarding reattach-to-user-namespace

--- modules/programs/tmux.nix
+++ modules/programs/tmux.nix
@@ -65,7 +65,7 @@
       ''
       + lib.optionalString pkgs.stdenv.isDarwin ''
         # Fix copy-mode on MacOS
-        set-option -g default-command "${lib.getExe pkgs.reattach-to-user-namespace} -l $SHELL"
+        set-option -g default-command "${lib.getExe' pkgs.reattach-to-user-namespace "reattach-to-user-namespace"} -l $SHELL"
       '';
     keybinds = ''
       # Unbind
